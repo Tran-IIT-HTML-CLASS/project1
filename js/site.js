18c575b8f3cdf02ef3846dadc4b7ab0e786eed79
Henry T <htran21@hawk.iit.edu>
2025-03-25 20:32:20 -0500

    testing required keyword

--- js/site.js
+++ js/site.js
@@ -3,18 +3,11 @@ jQuery(function($) {
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
 
-  // Add confirmation before form submission
-  $('form').on('submit', function(e) {
-    let confirmSubmit = confirm('Are you sure you want to submit the form?');
-    if (!confirmSubmit) {
-      e.preventDefault();
-    }
-  });
-
-  // Disable submit until all required fields are filled
+  // Select the submit button
   let $submitButton = $('#submit input[type="submit"]');
-  $submitButton.prop('disabled', true);
+  $submitButton.prop('disabled', true); // Disable button initially
 
+  // Enable submit when all required fields are filled
   $('form :input[required]').on('input', function() {
     let allFilled = true;
     $('form :input[required]').each(function() {
@@ -22,6 +15,14 @@ jQuery(function($) {
         allFilled = false;
       }
     });
-    $submitButton.prop('disabled', !allFilled);
+    $submitButton.prop('disabled', !allFilled); // Enable button if all fields are filled
+  });
+
+  // Add confirmation before form submission
+  $('form').on('submit', function(e) {
+    let confirmSubmit = confirm('Are you sure you want to submit the form?');
+    if (!confirmSubmit) {
+      e.preventDefault(); // Prevent submission if user clicks "Cancel"
+    }
   });
 });
