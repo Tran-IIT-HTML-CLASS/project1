e0b69ee3fc37ee3b65c53776a455aae2d98e5e49
Henry T <htran21@hawk.iit.edu>
2025-03-25 20:28:03 -0500

    testing javascript

--- js/site.js
+++ js/site.js
@@ -1,5 +1,27 @@
-//Nothing here yet, just adding javascript file
 jQuery(function($) {
+  // Change class to indicate JS is enabled
   $('html').removeClass('nojs');
   $('html').addClass('hasjs');
+
+  // Add confirmation before form submission
+  $('form').on('submit', function(e) {
+    let confirmSubmit = confirm('Are you sure you want to submit the form?');
+    if (!confirmSubmit) {
+      e.preventDefault();
+    }
+  });
+
+  // Disable submit until all required fields are filled
+  let $submitButton = $('#submit input[type="submit"]');
+  $submitButton.prop('disabled', true);
+
+  $('form :input[required]').on('input', function() {
+    let allFilled = true;
+    $('form :input[required]').each(function() {
+      if ($(this).val().trim() === '') {
+        allFilled = false;
+      }
+    });
+    $submitButton.prop('disabled', !allFilled);
+  });
 });
